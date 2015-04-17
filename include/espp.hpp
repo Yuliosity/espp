c83d9ba1a26fe24556178b1a4f2c2ee62fe57ae6
Yury Gorshkov <yuuri@yuuri.me>
2015-04-18 01:05:03 +0300

    Initial commit

---
+++ include/espp.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Yury on 18/04/15.
+//
+
+#ifndef ESPP_ESPP_HPP
+#define ESPP_ESPP_HPP
+
+#include
+
+#endif //ESPP_ESPP_HPP
---
+++ include/request.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Yury on 18/04/15.
+//
+
+#ifndef ESPP_REQUEST_HPP
+#define ESPP_REQUEST_HPP
+
+class Request
+{
+
+};
+
+#endif //ESPP_REQUEST_HPP
